24cce7512135e92cbb6dd5cfc5206bfb5bb18162
Irene Chelangat <irenechelangat238@gmail.com>
2026-04-20 15:59:53 +0300

    Completed JS functions lab

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+// Function 1
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Function 2
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Function 3
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+// Function 4
+function isPalindrome(word) {
+  const lowerWord = word.toLowerCase();
+  const reversed = lowerWord.split('').reverse().join('');
+  return lowerWord === reversed;
+}
+
+// Function 5
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
+
+
+// DO NOT TOUCH THIS
+module.exports = { 
+  calculateTax, 
+  convertToUpperCase, 
+  findMaximum, 
+  isPalindrome, 
+  calculateDiscountedPrice 
+};
 
 
 
